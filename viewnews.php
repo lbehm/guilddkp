f65eb7b30d3eb8207993b43d3edb75ad3cec1632
devimplode <requiem.inferna@googlemail.com>
2010-10-03 03:25:39 +0200

    Update: viewnews.php - Bug-Fixes

--- viewnews.php
+++ viewnews.php
@@ -24,40 +24,19 @@ if ($newsid)
 }
 else
 {
-	$previous_date = null;
-	$sql = 'SELECT n.*, u.user_displayname
-			FROM ' . T_NEWS . ' n, ' . T_USER . ' u
-			WHERE (n.user_id = u.user_id)
-			ORDER BY news_flags DESC, news_date DESC LIMIT ' . $start . ', '.$newsPerPage;
+	$sql = 'SELECT * FROM ' . T_NEWS . ' ORDER BY news_flags DESC, news_date DESC LIMIT ' . $start . ', '.intval($newsPerPage).';';
 	$result = $db->query($sql);
-	
-
 	if ( $db->num_rows($result) == 0 )
 	{
-		$sql = 'SELECT n.news_id, n.news_date, n.news_headline, n.news_message, n.user_name, u.user_displayname
-				FROM ' . T_NEWS . ' n, ' . T_USER . ' u
-				WHERE (n.user_id = u.user_id)
-				ORDER BY news_date DESC LIMIT ' . $start . ', '.$newsPerPage;
-		$result = $db->query($sql);
-		if ( $db->num_rows($result) == 0 )
-		{
-			die("Keine News");
-		}
+		die("Keine News");
 	}
 }
 $cur_news_number = 0;
 $sticky_news = 0;
 $news_array = array();
-	   
 while( $news = $db->fetch_record($result) )
-{	
+{
 	$message = $news['news_message'];
-
-	//Details views
-	if(($newsid) and (strlen($news['extended_message'])>1))
-	{
-		$message .= "[br][br]".$news['extended_message'];
-	}
 
 	$SHOWCOMMENT = false;
 	/*
@@ -73,10 +52,10 @@ while( $news = $db->fetch_record($result) )
 	}
 	*/
 	$t = time()-$news['news_date'];
-	$t = ($t<60) ? "Vor weniger als einer Minute" : (($t<3600)?"Vor ".date("i",$t)." Minuten":date("G:i:s - d.m.Y"));
+	$t = ($t<60) ? "Vor weniger als einer Minute" : (($t<3600)?"Vor ".date("i",$t)." Minuten":date("G:i:s - d.m.Y", $news['news_date']));
 	$news_array[] = array(
 		'HEADLINE' => stripslashes($news['news_headline']),
-		'AUTHOR' => $news['user_displayname'],
+		'AUTHOR' => $news['user_name'],
 		'TIME' => $t,
 		'ID' => $news['news_id'],
 		'DETAIL' => ($newsid > 0 ) ? true : false,
@@ -85,16 +64,13 @@ while( $news = $db->fetch_record($result) )
 		'COMMENT'  => $COMMENT,
 		'MESSAGE' => $message,
 		'news' => $news);
-		
-}			
+}
 
 $db->free_result($result);	
 //ok now lets check the cached array
 foreach ($news_array as $news)
 {
 #	if($user->data['user_rank'] < $news['news']['news_permissions'])
-#		continue;
-	
 	$tpl->append('news_obj', 
 		array(
 			'STICKY' => ($news['news']['news_flags']) ? true : false,
